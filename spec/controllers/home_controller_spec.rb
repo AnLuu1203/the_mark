900fddde511dc8dfa5b233e30b0308bb18d3cb17
An Luu <lta.anluu@gmail.com>
2017-03-29 22:39:38 +0700

    home controller spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
+  describe 'GET #index' do
+
+    before { get :index }
+
+    it { expect(response).to render_template(:index) }
+
+  end
+
 end

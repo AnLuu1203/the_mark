9bf95d115426c13bbd0293ca4ad7a8c2776814da
An Luu <lta.anluu@gmail.com>
2017-03-30 21:06:45 +0700

    Fix mailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,17 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'ec-the-mark.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://ec-the-mark.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['GMAIL_ACCOUNT'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
